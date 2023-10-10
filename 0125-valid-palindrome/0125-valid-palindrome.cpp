def55bac74a78d91b8cb549055f078ee998a1593
Daniel Isaí Catonga Tecla <94944243+g0ldark@users.noreply.github.com>
2023-10-10 16:06:06 -0600

    Time: 4 ms (79.97%), Space: 7.5 MB (55.05%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        vector<char> new_s;
-        for(auto &l:s){
-            l = tolower(l);
-            if(l>=97 && l<=122 || l>=48 && l<=57)
-                new_s.push_back(l);
-        } 
-        int n,p2=new_s.size();
-        n = p2/2;
-        if(p2%2!=0)
-            n++;
-        p2--;
-        for(int p1=0;p1<n;p1++){
-            if(new_s[p1]!=new_s[p2-p1])
-                return false;
+        for(int p1=0, p2=s.size()-1;p1<p2;p1++,p2--){
+            while(isalnum(s[p1])==false && p1<p2){
+                p1++;
+            }
+            while(isalnum(s[p2])==false && p1<p2){
+                p2--;
+            }
+            if(tolower(s[p1])!=tolower(s[p2])) return false;
         }
-        
         return true;
     }
 };
