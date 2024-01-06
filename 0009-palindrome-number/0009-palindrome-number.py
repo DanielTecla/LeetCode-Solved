f78dff69392231f3c6d6ee5c231f20fcf2f1f1a3
Daniel Isaí Catonga Tecla <94944243+g0ldark@users.noreply.github.com>
2024-01-06 15:23:58 -0600

    Time: 54 ms (79.71%), Space: 17.2 MB (20.92%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -5,13 +5,12 @@ class Solution:
         
         x_ = x
         reverse_x = 0
-        while x_ is not 0:
+        while x_ != 0:
             temporal = x_ % 10
             x_ = x_//10
-            reverse_x = reverse_x * 10 + temporal
+            reverse_x = (reverse_x) * 10 + temporal
 
-        if x is not reverse_x:
+        print(f"{reverse_x}")
+        if x != reverse_x:
             return False
-
         return True
-        
