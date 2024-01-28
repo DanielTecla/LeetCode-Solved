dcbf435c2c299791d49df7a8e5a7e6d62cb93e47
Daniel Isaí Catonga Tecla <94944243+g0ldark@users.noreply.github.com>
2024-01-27 18:50:18 -0600

    Time: 30 ms (91.01%), Space: 16.7 MB (53.65%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -0,0 +1,12 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        last_word = 0
+        count = 0
+        for l in s:
+            if l == ' ':
+                count = 0
+            else: 
+                count += 1
+                last_word = count
+
+        return last_word
