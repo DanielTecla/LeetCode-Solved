2adc87d826c4656b1a4db670842854d01276d4e7
Daniel Isaí Catonga Tecla <94944243+g0ldark@users.noreply.github.com>
2024-02-04 10:11:05 -0600

    Time: 375 ms (62.65%), Space: 30.9 MB (97.09%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -10,10 +10,8 @@ class Solution:
         for i in range(1,len(nums)):
             if(nums[i] - nums[i-1] == 1):
                 temp += 1
-                long_seq = max(long_seq,temp)
-            elif(nums[i] - nums[i-1] == 0):
-                pass
-            else:
-                temp = 1
+            elif(nums[i] != nums[i-1]):
+                temp = 1            
+            long_seq = max(long_seq,temp)
         
         return long_seq
